37509a529fb248f72b453b1a89be89c680b36955
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-10-20 22:58:13 +0900

    solve 027

---
+++ src/bin/027.rs
@@ -0,0 +1,57 @@
+use std::collections::HashSet;
+
+#[allow(unused_macros)]
+macro_rules! parse_line {
+    ( $t:ty ) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            iter.next().unwrap().parse::<$t>().unwrap()
+        }
+    );
+
+    ( $( $t:ty), +) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            ( $(iter.next().unwrap().parse::<$t>().unwrap()),* )
+        }
+    );
+}
+
+#[allow(unused_macros)]
+macro_rules! read_line {
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! parse_vec {
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
+}
+
+fn main() {
+    let n = parse_line!(usize);
+    let mut name = HashSet::new();
+    let mut ans = 0;
+    for i in 0..n {
+        let acc = parse_line!(String);
+        let x = name.contains(&acc);
+        if !x {
+            name.insert(acc);
+            ans = i + 1;
+            println!("{}", ans);
+        }
+    }
+}
