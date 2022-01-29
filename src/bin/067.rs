8fbdf7ee7050e63fdd0fd9670f6c264732989636
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2022-01-29 17:11:55 +0900

    solve 067

--- src/bin/067.rs
+++ src/bin/067.rs
@@ -21,28 +21,36 @@ macro_rules! parse_line {
 
 #[allow(unused_macros)]
 macro_rules! read_line {
-    () => (
-        {
-            let mut line = String::new();
-            ::std::io::stdin().read_line(&mut line).unwrap();
-            line.pop();
-            line
-        }
-    )
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
 }
 
 #[allow(unused_macros)]
 macro_rules! parse_vec {
-    ( $t:ty ) => (
-        {
-            let mut line = String::new();
-            ::std::io::stdin().read_line(&mut line).unwrap();
-            let iter = line.split_whitespace();
-            iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
-        }
-    )
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
 }
 
 fn main() {
-    unimplemented!();
+    let (n, k) = parse_line!(String, usize);
+    let mut n = usize::from_str_radix(&n, 8).unwrap();
+    for _ in 0..k {
+        let mut nn = 0usize;
+        let mut oc = 1usize;
+        while n > 0 {
+            nn += oc * if n % 9 == 8 { 5 } else { n % 9 };
+            n /= 9;
+            oc *= 8;
+        }
+        n = nn;
+    }
+    println!("{:o}", n);
 }
