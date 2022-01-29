07be2ac15542e00892bcf1b74b174288178af850
KatsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2022-01-29 15:49:46 +0900

    solve 061

--- src/bin/061.rs
+++ src/bin/061.rs
@@ -1,3 +1,5 @@
+use std::{collections::VecDeque, convert::TryInto};
+
 #[allow(unused_macros)]
 macro_rules! parse_line {
     ( $t:ty ) => (
@@ -21,28 +23,35 @@ macro_rules! parse_line {
 
 #[allow(unused_macros)]
 macro_rules! read_line {
-    () => (
-        {
-            let mut line = String::new();
-            ::std::io::stdin().read_line(&mut line).unwrap();
-            line.pop();
-            line
-        }
-    )
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
 }
 
 #[allow(unused_macros)]
 macro_rules! parse_vec {
-    ( $t:ty ) => (
-        {
-            let mut line = String::new();
-            ::std::io::stdin().read_line(&mut line).unwrap();
-            let iter = line.split_whitespace();
-            iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
-        }
-    )
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
 }
 
 fn main() {
-    unimplemented!();
+    let q = parse_line!(usize);
+    let mut deck = VecDeque::new();
+    for _ in 0..q {
+        let (t, x) = parse_line!(usize, i64);
+        if t == 1 {
+            deck.push_front(x);
+        } else if t == 2 {
+            deck.push_back(x);
+        } else if t == 3 {
+            println!("{}", deck[(x - 1).try_into().unwrap()]);
+        }
+    }
 }
